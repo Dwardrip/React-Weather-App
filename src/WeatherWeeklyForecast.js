001ddfcdbae14906ba79a144a49614ac5c6434ff
Danielle <130064505+Dwardrip@users.noreply.github.com>
2023-07-08 14:20:38 +0200

    Update WeatherWeeklyForecast.js

--- src/WeatherWeeklyForecast.js
+++ src/WeatherWeeklyForecast.js
@@ -2,33 +2,32 @@ import React from "react";
 import "./index.css";
 
 export default function WeatherWeeklyForecast(props) {
-    function day() {
-        let date = new Date(props.data.time*1000);
-        let day = date.getDay();
-        let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-        return days[day];
-    }
+  function day() {
+    let date = new Date(props.data.time * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[day];
+  }
 
-    function maxTemperature() {
-        let temperature = Math.round(props.data.temperature.maximum);
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temperature.maximum);
 
-        return `${temperature} °C`
-    }
+    return `${temperature} °C`;
+  }
 
-    function minTemperature() {
-        let temperature = Math.round(props.data.temperature.minimum);
+  function minTemperature() {
+    let temperature = Math.round(props.data.temperature.minimum);
 
-        return `${temperature} °C`
-    }
+    return `${temperature} °C`;
+  }
 
-    return (
-        <div className="WeatherWeeklyForecast">
-            <div className="weather-forecast-date">{day()}</div>
-            <Weather iconUrl={props.data.weather[0].icon} size={38} />
-            <div className="forecast-temperature">
-                <span className="forecast-temperature-max">{maxTemperature()}</span>
-                <span className="forecast-temperature-min">{minTemperature()}</span>
-            </div>
-        </div>
-    );
+  return (
+    <div className="WeatherWeeklyForecast">
+      <div className="weather-forecast-date">{day()}</div>
+      <div className="forecast-temperature">
+        <span className="forecast-temperature-max">{maxTemperature()}</span>
+        <span className="forecast-temperature-min">{minTemperature()}</span>
+      </div>
+    </div>
+  );
 }
