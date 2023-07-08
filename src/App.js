dfd81aa63fbb9a7bde6df6d090876f42d7ff0835
Danielle <130064505+Dwardrip@users.noreply.github.com>
2023-07-08 14:18:07 +0200

    fixing errors

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
 import Weather from "./Weather";
-import Date from "./Date";
+import FormattedDate from "./FormattedDate";
 
 import "./index.css";
 
 export default function App() {
   return (
-          <div className="App">
-        <header className="App-header">
-          <Date />
-          <Weather city="Gingelom"/>
-        </header>
-      </div>
-      
-    );
-  }
+    <div className="App">
+      <header className="App-header">
+        <FormattedDate date={new Date()} />
+        <Weather city="Gingelom" />
+      </header>
+    </div>
+  );
+}
